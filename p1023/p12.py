8a1eaf31634f00859d615d65b8539f9acdc48da9
rladudals1234 <goadiddl@naver.com>
2025-10-23 18:25:31 +0900

    p12 일부 수정 > print에 쉼표추가

--- p1023/p12.py
+++ p1023/p12.py
@@ -1,6 +1,6 @@
 # 국어점수, 영어점수, 수학점수를 입력받아 평균,합계를 출력하시오.
-num1 = int(input("국어점수 입력하세요: "))
-num2 = int(input("영어점수 입력하세요: "))
-num3 = int(input("수학점수 입력하세요: "))
-print("평균:"+str((num1+num2+num3)/3))
-print("합계:"+str(num1+num2+num3))
+num1 = int(input("국어점수를 입력하세요: "))
+num2 = int(input("영어점수를 입력하세요: "))
+num3 = int(input("수학점수를 입력하세요: "))
+print("합계:", num1+num2+num3)  #쉼표사용자를 사용하면 타입이 달라도 된다.
+print("평균:", (num1+num2+num3)/3)
