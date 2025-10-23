1dd22291cebb8e38008cabf85d07a9c261697178
rladudals1234 <goadiddl@naver.com>
2025-10-23 16:25:17 +0900

    p07.py등록

--- p1023/p06_타입.py
+++ p1023/p06_타입.py
@@ -11,4 +11,5 @@ num5 = False    # bool
 num7 = "true"   # 타입 - 문자열
 num8 = None
 #print(num1+num2)   # 문자열,숫자는 덧셈이 안됨.
+print(num8)
 print(type(num8))   # str-> String, type()변수의 타입을 확인할 수 있다.
---
+++ p1023/p07.py
@@ -0,0 +1,10 @@
+print("퀴즈")
+print("1. num 변수 100 저장하고 num출력하시오.")
+num = 100
+print("결과: "+str(num))
+
+print("2. result변수에 123 * 5678값을 저장하고 result를 출력하시오.")
+result = 123 * 5678
+print("결과: "+str(result))
+print("3. result값에 56789를 곱해서 출력하시오.")
+print("결과: "+str(result * 56789))
